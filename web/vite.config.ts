5beee66746f8f146521460361d79bdd2343f3230
Matthew Martin <matthewdeanmartin@gmail.com>
2026-03-19 16:20:23 -0400

    0.1.6 web version

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',
   publicDir: 'public',
+  base: '/safari_writer/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
